62add8d2b351a46dbbf48fe882f202191220ce77
HIMANSHU2307 <hvs2394@gmail.com>
2019-07-26 12:05:55 +0530

    Heroku Commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/halanxStore'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/halanxStore/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
